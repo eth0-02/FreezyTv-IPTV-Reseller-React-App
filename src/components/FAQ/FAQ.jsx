8b16cc29d937bd8f6a775a4791833b660c7e8e08
ethoX <58401466+eth0-02@users.noreply.github.com>
2024-08-05 13:14:59 +0300

    pix

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -11,7 +11,7 @@ const FAQ = () => {
         <div className='faq'>   
             <p className="subheading">TESTIMONIALS</p>
             <h3 className="heading">What our clients say</h3>
-            <Collapsible className='faq_collapsible' toggletitle="How Many Channel Do I Get?" toggledescription="Pixsmartv Provides around 20000+ live TV channels from the world in local and international languages. Instead of this, a user will also get 60,000 to 110,000+ VODS including TV Series, Movies, Dramas and so on."/>
+            <Collapsible className='faq_collapsible' toggletitle="How Many Channel Do I Get?" toggledescription="Freezytv Provides around 20000+ live TV channels from the world in local and international languages. Instead of this, a user will also get 60,000 to 110,000+ VODS including TV Series, Movies, Dramas and so on."/>
             <Collapsible className='faq_collapsible' toggletitle="What Is Our Quality Servers?" toggledescription="We are first the best service provider on internet, and we provide no freezing technology with 99% uptime."/>
             <Collapsible className='faq_collapsible' toggletitle="What Types Of Devices Are Supported?" toggledescription="We support all kind of devices such as: MAG, Android Smart Phone, Android Boxes, Enigma, DreamBox, Vu+ (…), PC, VLC, Kodi/XBMC (…), Smart TV."/>
             <Collapsible className='faq_collapsible' toggletitle="How Will I Receive My Subscription?" toggledescription="After making a payment you’ll be contacted via email in less than 30 minutes with your logins credentials."/>
